396a9e93338c2e4d8538612aff5d17bfb900ce19
Vineet0105 <vineetpokharna7@gmail.com>
2025-10-25 17:17:51 +0530

    First Commit

---
+++ courses/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    students = models.ManyToManyField(Student, related_name='courses', blank=True)
+
+    def __str__(self):
+        return self.title
